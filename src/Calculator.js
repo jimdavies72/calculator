0eba63400189e9f74e76c5de65341588968aac58
James Davies <60436624+jimdavies72@users.noreply.github.com>
2021-12-17 09:05:06 +0000

    styling complete

--- src/Calculator.js
+++ src/Calculator.js
@@ -4,31 +4,99 @@ import { evaluate } from "mathjs";
 import { useRef } from "react";
 
 const Calculator = () => {
-  const [displayText, setDisplayText] = useState("");
+  const [displayText, setDisplayText] = useState("0");
   const [keyDigits, setKeyDigits] = useState([
-    "0",
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    ".",
-    "AC",
-    "*",
-    "/",
-    "+",
-    "-",
-    "=",
+    {
+      digit: "1",
+      id: "one",
+      lz: false,
+    },
+    {
+      digit: "2",
+      id: "two",
+      lz: false,
+    },
+    {
+      digit: "3",
+      id: "three",
+      lz: false,
+    },
+    {
+      digit: "+",
+      id: "plus",
+      lz: false,
+    },
+    {
+      digit: "4",
+      id: "four",
+      lz: false,
+    },
+    {
+      digit: "5",
+      id: "five",
+      lz: false,
+    },
+    {
+      digit: "6",
+      id: "six",
+      lz: false,
+    },
+    {
+      digit: "-",
+      id: "minus",
+      lz: false,
+    },
+    {
+      digit: "7",
+      id: "seven",
+      lz: false,
+    },
+    {
+      digit: "8",
+      id: "eight",
+      lz: false,
+    },
+    {
+      digit: "9",
+      id: "nine",
+      lz: false,
+    },
+    {
+      digit: "*",
+      id: "times",
+      lz: false,
+    },
+    {
+      digit: "0",
+      id: "zero",
+      lz: false,
+    },
+    {
+      digit: ".",
+      id: "point",
+      lz: true,
+    },
+    {
+      digit: "AC",
+      id: "clear",
+      lz: false,
+    },
+    {
+      digit: "/",
+      id: "divide",
+      lz: false,
+    },
+    {
+      digit: "=",
+      id: "equals",
+      lz: false,
+    },
   ]);
 
   const mainRef = useRef(null);
 
   const keyDownHandler = (e) => {
-    if (keyDigits.includes(e.key)) {
+    if (keyDigits.digit.includes(e.key)) {
       let newString = displayText + e.key;
       setDisplayText(newString);
     } else if (e.key === "Enter") {
@@ -39,10 +107,10 @@ const Calculator = () => {
   };
 
   const enterDigitHandler = (index) => {
-    if (keyDigits[index] !== "=" && keyDigits[index] !== "AC") {
-      let newString = displayText + keyDigits[index];
+    if (keyDigits[index].digit !== "=" && keyDigits[index].digit !== "AC") {
+      let newString = displayText + keyDigits[index].digit;
       setDisplayText(newString);
-    } else if (keyDigits[index] === "=") {
+    } else if (keyDigits[index].digit === "=") {
       //evaluate maths
       setDisplayText(evaluateMath(displayText));
     } else {
@@ -52,7 +120,6 @@ const Calculator = () => {
   };
 
   const evaluateMath = (mathString) => {
-    console.log(evaluate(3 / 0));
     let evaluation = evaluate(mathString);
     if (evaluation === Infinity) {
       return "Err";
@@ -67,28 +134,36 @@ const Calculator = () => {
       onKeyDown={keyDownHandler}
       className="container"
     >
-      <>
+      <div className="manu">
+        <h2>CASIO</h2>
+        <h5>MS-120BM</h5>
+      </div>
+      <div className="display">
         <h1>{displayText}</h1>
-      </>
-
-      {keyDigits.map((digit, index) => {
-        return (
-          <Digit
-            key={index}
-            digit={digit}
-            enterDigit={() => enterDigitHandler(index)}
-          />
-        );
-      })}
+      </div>
+      <div className="digits">
+        {keyDigits.map((digit, index) => {
+          return (
+            <Digit
+              key={index}
+              digit={digit.digit}
+              id={digit.id}
+              enterDigit={() => enterDigitHandler(index)}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 };
 
 const Digit = (props) => {
   return (
-    <>
-      <h2 onClick={props.enterDigit}>{props.digit}</h2>
-    </>
+    <div className="button">
+      <h2 id={props.id} onClick={props.enterDigit}>
+        {props.digit}
+      </h2>
+    </div>
   );
 };
 
